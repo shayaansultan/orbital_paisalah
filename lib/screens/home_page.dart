6febedd1ede52866bb04c2f75dc3f0209d1c6bfe
Shayaan Sultan <sultan.shayaan@gmail.com>
2023-05-23 17:42:06 +0500

    Updated home_page, work in progress login_page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,68 +10,119 @@ class HomePage extends StatelessWidget {
         color: const Color.fromARGB(255, 24, 51, 81),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             //SizedBox(height: 100), // Add spacing to move the Logo down
-            Center(child: Image(image: AssetImage('lib/assets/Logo.png'))),
-            SizedBox(height: 20), // Add spacing between the Logo and text
-            Text(
+            const Center(
+                child: Image(image: AssetImage('lib/assets/Logo.png'))),
+            //SizedBox(height: 10), // Add spacing between the Logo and text
+            const Text(
               'PaisaLah!',
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
               ),
             ),
-            SizedBox(height: 10), // Add spacing between the title and caption
-            Text(
+            const SizedBox(
+                height: 10), // Add spacing between the title and caption
+            const Text(
               'Your one-stop personal finance solution',
               style: TextStyle(
                 fontSize: 16,
-                color: Colors.grey,
+                color: Color.fromARGB(255, 255, 255, 255),
               ),
             ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            OutlinedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color.fromARGB(255, 24, 51, 81)),
-                minimumSize: MaterialStateProperty.all<Size>(Size(100, 40)),
-                side: MaterialStateProperty.all<BorderSide>(
-                    BorderSide(color: Colors.white)),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              child: const Text('Login', style: TextStyle(color: Colors.white)),
-            ),
-            OutlinedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color.fromARGB(255, 24, 51, 81)),
-                minimumSize: MaterialStateProperty.all<Size>(Size(100, 40)),
-                side: MaterialStateProperty.all<BorderSide>(
-                    const BorderSide(color: Colors.white)),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterPage()),
-                );
-              },
-              child:
-                  const Text('Register', style: TextStyle(color: Colors.white)),
-            ),
+            const SizedBox(
+              height: 50,
+            ), // Add spacing between the caption and buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 24, 51, 81)),
+                    minimumSize:
+                        MaterialStateProperty.all<Size>(const Size(100, 40)),
+                    side: MaterialStateProperty.all<BorderSide>(
+                        const BorderSide(color: Colors.white)),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  child: const Text('Login',
+                      style: TextStyle(color: Colors.white)),
+                ),
+                OutlinedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 24, 51, 81)),
+                    minimumSize: MaterialStateProperty.all<Size>(Size(100, 40)),
+                    side: MaterialStateProperty.all<BorderSide>(
+                        const BorderSide(color: Colors.white)),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => RegisterPage()),
+                    );
+                  },
+                  child: const Text('Register',
+                      style: TextStyle(color: Colors.white)),
+                )
+              ],
+            )
           ],
         ),
       ),
     );
   }
 }
+
+//   //   //   bottomNavigationBar: BottomAppBar(
+//   //   //     child: Row(
+//   //   //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//   //   //       children: [
+//   //   //         OutlinedButton(
+//   //   //           style: ButtonStyle(
+//   //   //             backgroundColor: MaterialStateProperty.all<Color>(
+//   //   //                 const Color.fromARGB(255, 24, 51, 81)),
+//   //   //             minimumSize:
+//   //   //                 MaterialStateProperty.all<Size>(const Size(100, 40)),
+//   //   //             side: MaterialStateProperty.all<BorderSide>(
+//   //   //                 const BorderSide(color: Colors.white)),
+//   //   //           ),
+//   //   //           onPressed: () {
+//   //   //             Navigator.push(
+//   //   //               context,
+//   //   //               MaterialPageRoute(builder: (context) => LoginPage()),
+//   //   //             );
+//   //   //           },
+//   //   //           child: const Text('Login', style: TextStyle(color: Colors.white)),
+//   //   //         ),
+//   //   //         OutlinedButton(
+//   //   //           style: ButtonStyle(
+//   //   //             backgroundColor: MaterialStateProperty.all<Color>(
+//   //   //                 const Color.fromARGB(255, 24, 51, 81)),
+//   //   //             minimumSize: MaterialStateProperty.all<Size>(Size(100, 40)),
+//   //   //             side: MaterialStateProperty.all<BorderSide>(
+//   //   //                 const BorderSide(color: Colors.white)),
+//   //   //           ),
+//   //   //           onPressed: () {
+//   //   //             Navigator.push(
+//   //   //               context,
+//   //   //               MaterialPageRoute(builder: (context) => RegisterPage()),
+//   //   //             );
+//   //   //           },
+//   //   //           child:
+//   //   //               const Text('Register', style: TextStyle(color: Colors.white)),
+//   //   //         ),
+//   //   //       ],
+//   //   //     ),
+//   //   //   ),
+//   //   // );
+//   // }
+// }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,15 +1,69 @@
+// import 'package:flutter/material.dart';
+
+// class LoginPage extends StatefulWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         backgroundColor: const Color.fromARGB(255, 24, 51, 81),
+//         appBar: AppBar(
+//           backgroundColor: const Color.fromARGB(255, 24, 51, 81),
+//           shadowColor: Colors.transparent,
+//         ),
+//         body: Container());
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return (Scaffold(
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 24, 51, 81),
       appBar: AppBar(
-        title: Text('Login'),
+          //title: Text('Login Page'),
+          ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              controller: _emailController,
+              decoration: const InputDecoration(
+                  labelText: 'Email', hoverColor: Colors.white),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                String email = _emailController.text;
+                String password = _passwordController.text;
+                // Perform login logic here
+                print('Email: $email');
+                print('Password: $password');
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text('Login Page'),
-      ),
-    ));
+    );
   }
 }
