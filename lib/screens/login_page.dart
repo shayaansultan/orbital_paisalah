6337d0475464aa33e0c2dcb051cb1a9ac0601699
Shayaan Sultan <sultan.shayaan@gmail.com>
2023-05-24 11:30:21 +0500

    Login Page Frontend Done. Register Page Updated

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -20,21 +20,59 @@ class _LoginPageState extends State<LoginPage> {
           shadowColor: Colors.transparent,
         ),
         body: Padding(
-          padding: EdgeInsets.all(40),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            TextField(
-              cursorColor: Colors.white,
+          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 40.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Title(
+                color: Colors.white,
+                child: const Text("Login",
+                    style: TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                    textAlign: TextAlign.center)),
+            const SizedBox(height: 50),
+            TextFormField(
               controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: "Email",
-                labelStyle: TextStyle(color: Colors.white),
-                iconColor: Colors.white,
-                focusColor: Colors.white,
-                hoverColor: Colors.white,
-                fillColor: Colors.white,
-              ),
               style: const TextStyle(color: Colors.white),
-            )
+              decoration: const InputDecoration(
+                label: Text("Enter Email"),
+                labelStyle: TextStyle(color: Colors.white),
+                prefixIcon: Icon(Icons.email_outlined),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                prefixIconColor: Colors.white,
+                suffixIconColor: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              controller: _passwordController,
+              style: const TextStyle(color: Colors.white),
+              obscureText: true,
+              decoration: const InputDecoration(
+                label: Text("Enter Password"),
+                labelStyle: TextStyle(color: Colors.white),
+                prefixIcon: Icon(Icons.password_outlined),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                prefixIconColor: Colors.white,
+                suffixIconColor: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text("Login"),
+              ),
+            ),
           ]),
         ));
   }
